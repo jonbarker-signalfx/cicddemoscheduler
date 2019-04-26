8317cb1671186ea306c1fdfde90c4b28656385fa
jonbarker-signalfx <49037298+jonbarker-signalfx@users.noreply.github.com>
2019-04-26 11:05:35 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
-extern crate cronjob;
-use cronjob::CronJob;
 use reqwest;
 use std::collections::HashMap;
-use std::time::Duration;
 use clokwerk::{Scheduler, TimeUnits};
-use clokwerk::Interval::*;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
 
-
-
 let mut scheduler = Scheduler::new();
-
-scheduler.every(1.minutes()).plus(30.seconds()).run(sendcurl);
-
-}
-
-fn sendcurl() {
+scheduler.every(5.minutes()).plus(30.seconds()).run(||{
     let mut map = HashMap::new();
     map.insert("user", "jbarker@signalfx.com");
-    map.insert("body", "json");
     let client = reqwest::Client::new();
-    let _res = client.post("https://us-central1-se-cicd-demo.cloudfunctions.net/deploy-canary")
+    client.post("https://us-central1-se-cicd-demo.cloudfunctions.net/deploy-canary")
     .json(&map)
     .send();
     println!("Curl request sent!");
+}
+);
+
+loop {
+    scheduler.run_pending();
+    thread::sleep(Duration::from_millis(100));
+}
+
 }
 
 
